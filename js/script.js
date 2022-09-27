e7b6437c4426e363e37c607450e62502a3099712
Marcin <45795107+grygus00@users.noreply.github.com>
2022-09-27 19:05:19 +0200

    Homepage first draft

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// When the user scrolls down 150px from the top of the document, resize the header's font size
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 250 ||
+    document.documentElement.scrollTop > 250
+  ) {
+    document.getElementById("logo_header_img").style.width = "5rem";
+    document.getElementById("logo").style.top = "20px";
+  } else {
+    document.getElementById("logo_header_img").style.width = "17rem";
+    document.getElementById("logo").style.top = "330px";
+  }
+}
+
+const scrollContainer = document.querySelector("#rent_list_home");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
