a9548f75d6267fc6504ccd968bcc715bbe88b0c1
Vili <veli0119@stud.kea.dk>
2022-09-28 16:55:08 +0200

    contact form

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,17 @@
 const form = document.querySelector("form");
-
 console.log("");
+
+console.log("form.elements:" + form.elements.length);
+document.querySelector("button").addEventListener("click", submitSearch);
+
+// const inputNameEl = document.querySelector("#name");
+
+// inputNameEl.addEventListener("blur", handleFormData);
+
+// function handleFormData(e) {
+//   // e.preventDefault();
+//   //alert("hey");
+//   console.log("form.elements: ", form.elements);
+//   console.log("form.elements.named.value", form.elements.named.value);
+//   console.log("form.elements.age.value:", form.elements.age.value);
+// }
