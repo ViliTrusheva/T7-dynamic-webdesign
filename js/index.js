b5c7d39bab6b8f9f040423efa86e486cef4297ae
Marcin <45795107+grygus00@users.noreply.github.com>
2022-09-28 17:47:48 +0200

    some changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-// const url = "https://kea22-427f.restdb.io/rest/bikinis?q={}&sort=Price&dir=1";
-const url = `https://kea22-427f.restdb.io/rest/bikinis?q={"Rent":true}`;
+const url = "https://kea22-427f.restdb.io/rest/bikinis";
 
 const options = {
   headers: {
@@ -25,21 +24,15 @@ function dataReceived(data) {
 }
 
 function showProduct(product) {
-  const template = document.querySelector("#product_list template").content;
+  const template = document.querySelector(".box1").content;
   const clone = template.cloneNode(true);
-  clone.querySelector(".products .product_name").textContent = product.Bikini;
-  clone.querySelector(
-    ".products .product_price"
-  ).textContent = `${product.Price}  DKK`;
+  clone.querySelector(".box1 a img").src = product.img_link;
+  console.log(product.img_link);
+  clone.querySelector(".box1 a p").textContent = product.Bikini;
 
-  // clone.querySelector(
-  //   ".products a"
-  // ).href = `product.html?q={%22_id%22%20:%20%22${product._id}%22}`
   clone.querySelector(".products a").href = `product.html?id=${product._id}`;
 
-  clone.querySelector(".products img").src = product.img_link;
-
-  const parent = document.querySelector("#product_list");
+  const parent = document.querySelector("#rent_list_home");
   parent.appendChild(clone);
 
   // const urlParams = new URLSearchParams(window.location.search);
